9f75225cc96d6296b8638a51aa29d78b4a29125d
“[suchith <[suchithhc.scs24@bmsce.ac.in]>
2025-07-16 01:26:43 +0530

    Update app.py: added JSON prediction endpoint and Prometheus metrics

--- app.py
+++ app.py
@@ -4,8 +4,11 @@ from fastapi.responses import HTMLResponse, JSONResponse
 from pydantic import BaseModel
 import pandas as pd
 import joblib
+from prometheus_fastapi_instrumentator import Instrumentator
+
 
 app = FastAPI()
+Instrumentator().instrument(app).expose(app)
 templates = Jinja2Templates(directory="templates")
 
 # Load model and expected column order
